8fce8d484a64d61dbf8ce35189855e8b833e2a1c
Jhonata Gomes <jhonatagomes@MacBook-Air-de-Jhonata.local>
2021-01-21 15:21:22 -0300

    webrtc videocall

---
+++ README.md
@@ -0,0 +1,7 @@
+# zoom-clone
+Instale peer global
+
+npm i -g peer
+
+Rode na porta 3001
+peerjs --port 3001
---
+++ src/app.js
@@ -0,0 +1,31 @@
+const { Socket } = require('dgram');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuid4V } = require('uuid')
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res, next) => {
+    res.redirect(`/${uuid4V()}`)
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+        
+        socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconnected', userId);
+        })
+    })
+
+})
+
+server.listen(3000);
