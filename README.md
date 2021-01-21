4501669bd0a07cd88c816d966ace70788c7a65c0
Jhonata Gomes <jhonata.fgomes@hotmail.com>
2021-01-21 17:43:29 -0300

    peerjs integrado ao servidor

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # zoom-clone
-Instale peer global
-
-npm i -g peer
-
-Rode na porta 3001
-peerjs --port 3001
--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const { v4: uuid4V } = require('uuid')
 
+const { PeerServer } = require('peer');
+const peerServer = PeerServer({ port: 3001, path: '/peerjs' });
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
